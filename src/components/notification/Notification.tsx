1c9945c55e0c2a54d1f20ca4c35748d4bed2a9c0
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-30 15:02:31 -0400

    styles: changed style imports and adjust naming to be compatible with modules.

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { NotificationsContext } from "../../context/NotificationsContext";
-import "./notification.module.css"
+import styles from "./notification.module.css"
 
 const Notification = () => {
   const { notification } = useContext(NotificationsContext);
@@ -11,11 +11,11 @@ const Notification = () => {
   
   const {message, type} = notification;
 
-  const className = `notification ${type.toLowerCase()}`;
+  const className = `${styles.notification} ${styles[type.toLowerCase()]}`;
 
   return (
     <div className={className}>
-      <p className="notification-message">{message}</p>
+      <p className={styles.notificationMessage}>{message}</p>
     </div>
   )
 }
--- src/components/triggerButton/TriggerButton.tsx
+++ src/components/triggerButton/TriggerButton.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { NotificationsContext } from "../../context/NotificationsContext";
-import "./triggerButton.module.css"
+import styles from "./triggerButton.module.css"
 
 const TriggerButton = () => {
   const {showNotification} = useContext(NotificationsContext);
@@ -12,7 +12,7 @@ const TriggerButton = () => {
 
   return (
     <button 
-    className="trigger-button"
+    className={styles.triggerButton}
     onClick={handleOnClick}
     >
       Show Custom Notification
