c1e988b6f5a39f77effaa3dea8b29bb48454ae15
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-30 13:52:26 -0400

    feature: create notification component and add styles.

---
+++ src/components/notification/Notification.tsx
@@ -0,0 +1,10 @@
+
+const Notification = () => {
+  return (
+    <div className="notification">
+      <p notification-message>This is a notification message.</p>
+    </div>
+  )
+}
+
+export default Notification;
