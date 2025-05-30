16a176267fee693e669f93e4d2739ff2431c527d
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-30 15:01:59 -0400

    wip: added first component to try them out.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Notification from './components/notification/Notification'
+import TriggerButton from './components/triggerButton/triggerButton'
+import { NotificationsProvider } from './context/NotificationsProvider'
 
 function App() {
 
   return (
-    <h1>Hello!</h1>
-  )
+    <NotificationsProvider>
+      <Notification />
+      <main>
+        <h2>Welcome to My App</h2>
+        <TriggerButton />
+      </main>
+    </NotificationsProvider>
+  );
 }
 
-export default App
+export default App;
