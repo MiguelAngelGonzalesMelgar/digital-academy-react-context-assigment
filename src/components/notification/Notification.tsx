6809a91db739ba03c470041070e2d2d5b266fb9f
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-30 14:20:44 -0400

    styles: change style to handle different types of notifications.

--- src/components/notification/Notification.tsx
+++ src/components/notification/Notification.tsx
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+import { NotificationsContext } from "../../context/NotificationsContext";
+import "./notification.module.css"
 
 const Notification = () => {
+  const { notification } = useContext(NotificationsContext);
+
+  if (!notification) {
+    return null;
+  }
+  
+  const {message, type} = notification;
+
+  const className = `notification ${type.toLowerCase()}`;
+
   return (
-    <div className="notification">
-      <p notification-message>This is a notification message.</p>
+    <div className={className}>
+      <p className="notification-message">{message}</p>
     </div>
   )
 }
