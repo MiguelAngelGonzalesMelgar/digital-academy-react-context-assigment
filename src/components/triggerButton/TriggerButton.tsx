4be3a53d6b3192166abd95c8765d8f157e3554fe
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-30 14:34:08 -0400

    feature: add context to trigger a custom message.

--- src/components/triggerButton/TriggerButton.tsx
+++ src/components/triggerButton/TriggerButton.tsx
@@ -1,8 +1,20 @@
-
+import { useContext } from "react";
+import { NotificationsContext } from "../../context/NotificationsContext";
+import "./triggerButton.module.css"
 
 const TriggerButton = () => {
+  const {showNotification} = useContext(NotificationsContext);
+
+  const handleOnClick = (): void => {
+
+    showNotification("This is a custom notification", "Custom");
+  }
+
   return (
-    <button className="trigger-button">
+    <button 
+    className="trigger-button"
+    onClick={handleOnClick}
+    >
       Show Custom Notification
     </button>
   )
